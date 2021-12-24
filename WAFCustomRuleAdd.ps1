5c4719cd1079da7cda4eb2e401fbd9e3c52b48ff
timkto <68362768+timkto@users.noreply.github.com>
2021-12-24 12:29:09 +0800

    Update WAFCustomRuleAdd.ps1

--- WAFCustomRuleAdd.ps1
+++ WAFCustomRuleAdd.ps1
@@ -30,33 +30,18 @@ $IsDisabled = if ('Disabled' -eq $status) {$true} else {$false}
 [string[]]$NewIPList = @()
 $IPList = import-csv $ip_list
 
-ForEach ($item in $IPList) {
-	$IPArrayList += $item.("public_ip")
-}
-
-$counter = 0
-if ($IPArrayList -ne $null) {
-	for($i=0; $i -lt $IPArrayList.length; $i+=10) {
-		$NewIPList += ,$IPArrayList[$i..($i+9)]
-		$counter += 1
-	}
-}
-Write-Output $NewIPList.GetType()
-
 if ('Add' -eq $operation_type) {
 	ForEach ($WAFPolicy in $waf_policy_list) {
 		Write-Output $WAFPolicy
+		az network front-door waf-policy rule create --name $custom_rule_name --priority $priority --rule-type $rule_type --action $action --resource-group $rsg_name --policy-name $WAFPolicy --disabled $IsDisabled --defer
 		
-		if ($NewIPList -ne $null) {
-			az network front-door waf-policy rule create --name $custom_rule_name --priority $priority --rule-type $rule_type --action $action --resource-group $rsg_name --policy-name $WAFPolicy --disabled $IsDisabled --defer
-			for(; $counter -gt 0; $counter-=1) {
-			Write-Output $NewIPList[$counter-1]
-				[string[]]$LastIP = @($NewIPList[$counter-1])
-				Write-Output $IPArrayList.GetType()
-				Write-Output "test kit"
-				Write-Output $LastIP.GetType()
-				Write-Output $LastIP
-				az network front-door waf-policy rule match-condition add --match-variable RemoteAddr --operator IPMatch --values $LastIP --negate false --name $custom_rule_name --resource-group $rsg_name --policy-name $WAFPolicy
+		$counter = 0
+		ForEach ($item in $IPList) {
+			$IPArrayList += $item.("public_ip")
+			$counter += 1
+			if ($counter -eq 100) {
+				az network front-door waf-policy rule match-condition add --match-variable RemoteAddr --operator IPMatch --values $IPArrayList --negate false --name $custom_rule_name --resource-group $rsg_name --policy-name $WAFPolicy
+				$counter = 0
 			}
 		}
 		
