36bc2ee9d28065a80d14350319d4e870040a06a5
timkto <68362768+timkto@users.noreply.github.com>
2021-12-24 11:20:42 +0800

    Update WAFCustomRuleAdd.ps1

--- WAFCustomRuleAdd.ps1
+++ WAFCustomRuleAdd.ps1
@@ -53,6 +53,7 @@ if ('Add' -eq $operation_type) {
 			
 			for(; $counter -gt 0; $counter-=1) {
 			Write-Output $NewIPList[$counter-1]
+				az network front-door waf-policy rule create --name $custom_rule_name --priority $priority --rule-type $rule_type --action $action --resource-group $rsg_name --policy-name $WAFPolicy --disabled $IsDisabled --defer
 				az network front-door waf-policy rule match-condition add --match-variable RemoteAddr --operator IPMatch --values $NewIPList[$counter-1] --negate false --name $custom_rule_name --resource-group $rsg_name --policy-name $WAFPolicy
 			}
 		}
