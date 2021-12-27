41186ba97605fefae65f6987bd8e4c660e968703
timkto <68362768+timkto@users.noreply.github.com>
2021-12-27 09:55:42 +0800

    Update WAFCustomRuleAdd.ps1

--- WAFCustomRuleAdd.ps1
+++ WAFCustomRuleAdd.ps1
@@ -32,22 +32,34 @@ $PriorityID = $priority
 $RuleName = $custom_rule_name
 $IPList = import-csv $ip_list
 $counter = 0
+$IPCount = 0
 
 if ('Add' -eq $operation_type) {
 	ForEach ($WAFPolicy in $waf_policy_list) {
 		Write-Output $WAFPolicy
-		az network front-door waf-policy rule create --name $custom_rule_name --priority $PriorityID --rule-type $rule_type --action $action --resource-group $rsg_name --policy-name $WAFPolicy --disabled $IsDisabled --defer
+		##az network front-door waf-policy rule create --name $custom_rule_name --priority $PriorityID --rule-type $rule_type --action $action --resource-group $rsg_name --policy-name $WAFPolicy --disabled $IsDisabled --defer
 		
-		$IPCount = 0
 		ForEach ($item in $IPList) {
 			$IPArrayList += $item.("public_ip")
 			$IPCount += 1
 			$counter += 1
 			
 			if ($IPCount -eq 100 -or $counter -eq $IPList.length) {
-				az network front-door waf-policy rule match-condition add --match-variable RemoteAddr --operator IPMatch --values $IPArrayList --negate false --name $custom_rule_name --resource-group $rsg_name --policy-name $WAFPolicy
+				##az network front-door waf-policy rule match-condition add --match-variable RemoteAddr --operator IPMatch --values $IPArrayList --negate false --name $custom_rule_name --resource-group $rsg_name --policy-name $WAFPolicy
 				$IPCount = 0
 				$IPArrayList = @();
+				Write-Output $counter
+			}
+			
+			if ($counter -eq 600) {
+				$PriorityID += 1
+				Write-Output $PriorityID.GetType()
+				$RuleName += $PriorityID
+				Write-Output "tempe"
+				Write-Output $PriorityID
+				Write-Output $RuleName
+				##az network front-door waf-policy rule create --name $RuleName --priority $PriorityID --rule-type $rule_type --action $action --resource-group $rsg_name --policy-name $WAFPolicy --disabled $IsDisabled --defer
+
 			}
 		}
 		
